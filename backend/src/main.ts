5e4f5512160bbd0305f42efaa794e47525f1b962
JALAL <jalaluddin0046356@gmail.com>
2025-10-21 21:57:50 +0600

    fix(repo): remove backend submodule and track files directly

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+  console.log(`Backend is running on: ${await app.getUrl()}`);
+}
+bootstrap()
